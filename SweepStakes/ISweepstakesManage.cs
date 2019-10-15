a457b11ff703683d57c69787a80d6ec6fe4e271e
jacob thompson <you@example.com>
2019-10-15 11:08:57 -0500

    added dictonary and user interface

---
+++ SweepStakes/ISweepstakesManage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    class ISweepstakesManage
+    {
+        interface ISweepstakesManager<T>
+        {
+            void InsertSweepstakes(SweepStakes sweepstakes);
+            SweepStakes GetSweepstakes();
+        }
+    }
+}
